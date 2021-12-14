1af33d66053083dc2000c54473f7c7cd4430c65d
Sperrhacke, Bastian <Bastian.Sperrhacke@otto.de>
2021-12-14 20:05:16 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# dependencycheck
+# dependencycheck 
+## description
+searches within the current directory recursively for (also transitive) dependency of your given parameter.
+
+## usage
+
+```
+check_dependencytree_for_lib.sh log4j
+```
---
+++ check_dependencytree_for_lib.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+libpattern=$1
+slash="/"
+clear
+if [[ -z "$libpattern" ]];
+then
+    echo "please provide a lib pattern."
+    exit 16
+fi
+find . -type f -name 'pom.xml' | sed -r 's|/[^/]+$||' |sort |uniq | grep -v '.terraform' | while IFS= read -r d; do
+	echo "mvn dependency:tree for $d"
+	STEP_BACK_COUNT=$(awk -F"${slash}" '{print NF-1}' <<< "${d}")
+	cd $d
+	mvn dependency:tree | grep $libpattern
+	for ((n=0;n<$STEP_BACK_COUNT;n++))
+	do
+		cd ..
+	done
+done
+exit 0
