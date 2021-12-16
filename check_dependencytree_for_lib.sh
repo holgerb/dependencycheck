9ff3b7b013c3cd19207e2eea021177003204f4bc
Holger Bergunde <holgerb@users.noreply.github.com>
2021-12-16 10:42:12 +0100

    chore(master): Formatting.

--- check_dependencytree_for_lib.sh
+++ check_dependencytree_for_lib.sh
@@ -2,26 +2,22 @@
 libpattern=$1
 slash="/"
 clear
-if [[ -z "$libpattern" ]];
-then
-    echo "please provide a lib pattern."
-    exit 16
+if [[ -z "$libpattern" ]]; then
+  echo "please provide a lib pattern."
+  exit 16
 fi
-find . -name 'pom.xml' -or -name 'build.gradle*' -type f | sed -r 's|/[^/]+$||' |sort |uniq | grep -v '.terraform' | while IFS= read -r d; do
-  STEP_BACK_COUNT=$(awk -F"${slash}" '{print NF-1}' <<< "${d}")
+find . -name 'pom.xml' -or -name 'build.gradle*' -type f | sed -r 's|/[^/]+$||' | sort | uniq | grep -v '.terraform' | while IFS= read -r d; do
+  STEP_BACK_COUNT=$(awk -F"${slash}" '{print NF-1}' <<<"${d}")
   cd $d
 
-	if [ -n "$(ls | grep -i "pom.xml")" ];
-	then
-  	echo "Analysing maven project: $d"
-  	mvn dependency:tree | grep $libpattern
+  if [ -n "$(ls | grep -i "pom.xml")" ]; then
+    echo "Analysing maven project: $d"
+    mvn dependency:tree | grep $libpattern
   else
-    if [ -z "$(which gradle)" ] ;
-    then
-      if [ -n "$(ls | grep -i "gradlew")" ];
-      then
-          	echo "Analysing gradle project: $d"
-          	./gradlew -q dependencies | grep $libpattern
+    if [ -z "$(which gradle)" ]; then
+      if [ -n "$(ls | grep -i "gradlew")" ]; then
+        echo "Analysing gradle project: $d"
+        ./gradlew -q dependencies | grep $libpattern
       else
         echo "Found gradle project but there is no local gradle installation and no wrapper for project: $d"
       fi
@@ -29,12 +25,11 @@ find . -name 'pom.xml' -or -name 'build.gradle*' -type f | sed -r 's|/[^/]+$||' 
       echo "Analysing gradle project: $d"
       gradle -q dependencies | grep $libpattern
     fi
-	fi
+  fi
 
-	for ((n=0;n<$STEP_BACK_COUNT;n++))
-	do
-		cd ..
-	done
+  for ((n = 0; n < $STEP_BACK_COUNT; n++)); do
+    cd ..
+  done
 
 done
 exit 0
