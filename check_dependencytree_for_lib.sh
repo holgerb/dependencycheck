ca9e9bb1379e2295684789053bb5deb4b3968990
Sperrhacke, Bastian <Bastian.Sperrhacke@otto.de>
2022-01-04 10:57:39 +0100

    Merge branch 'master' of https://github.com/holgerb/dependencycheck into holgerb-master
    # Conflicts:
    #	check_dependencytree_for_lib.sh

--- check_dependencytree_for_lib.sh
+++ check_dependencytree_for_lib.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 libpattern=$1
-rootdir=$2
 slash="/"
 clear
-if [[ -z "$libpattern" ]];
-then
-    echo "please provide a lib pattern."
-    exit 16
+if [[ -z "$libpattern" ]]; then
+  echo "please provide a lib pattern."
+  exit 16
 fi
-if [[ -z "$rootdir" ]];
-then
-    rootdir="./"
-fi
-cd $rootdir
-find . -type f -name 'pom.xml' | sed -r 's|/[^/]+$||' |sort |uniq | grep -v '.terraform' | while IFS= read -r d; do
-	echo "mvn dependency:tree for $d"
-	STEP_BACK_COUNT=$(awk -F"${slash}" '{print NF-1}' <<< "${d}")
-	cd $d
-	mvn dependency:tree | grep $libpattern
-	for ((n=0;n<$STEP_BACK_COUNT;n++))
-	do
-		cd ..
-	done
+find . -name 'pom.xml' -or -name 'build.gradle*' -type f | sed -r 's|/[^/]+$||' | sort | uniq | grep -v '.terraform' | while IFS= read -r d; do
+  STEP_BACK_COUNT=$(awk -F"${slash}" '{print NF-1}' <<<"${d}")
+  cd $d
+
+  if [ -n "$(ls | grep -i "pom.xml")" ]; then
+    echo "Analysing maven project: $d"
+    mvn dependency:tree | grep $libpattern
+  else
+    if [ -z "$(which gradle)" ]; then
+      if [ -n "$(ls | grep -i "gradlew")" ]; then
+        echo "Analysing gradle project: $d"
+        ./gradlew -q dependencies | grep $libpattern
+      else
+        echo "Found gradle project but there is no local gradle installation and no wrapper for project: $d"
+      fi
+    else
+      echo "Analysing gradle project: $d"
+      gradle -q dependencies | grep $libpattern
+    fi
+  fi
+
+  for ((n = 0; n < $STEP_BACK_COUNT; n++)); do
+    cd ..
+  done
+
 done
 exit 0
